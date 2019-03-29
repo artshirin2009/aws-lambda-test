04859f279f4279aeff1018258ad1d564c7ab5539
artshirin2009 <artshirin2009@gmail.com>
2019-03-29 12:43:09 +0200

    dsa

---
+++ aws-lambda/functions/hgetRedisFinal.js
@@ -0,0 +1,31 @@
+'use strict'
+require('dotenv').config();
+var mysql = require('promise-mysql');
+var redis = require('redis')
+
+module.exports.hgetRedisFinal = (event, context, callback) => {
+  const client = redis.createClient({ url: 'redis://mywebcahe1.fsgenx.0001.euc1.cache.amazonaws.com:6379' });
+  client.on("error", (err) => {
+    console.log('error there');
+    client.quit();
+    callback(err, null);
+  });
+
+  client.hgetall(event.pathParameters.hash, (err, result) => {
+    var arr = valuesToArray(result);
+    var finArr = []
+    arr.forEach(element => {
+      finArr.push(JSON.parse(element))
+    });
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(finArr)
+    }
+    client.quit();
+    callback(null, response);
+    function valuesToArray(obj) {
+      return Object.keys(obj).map(function (key) { return obj[key]; });
+    }
+  })
+
+}
--- aws-lambda/functions/hsetRedisFinal.js
+++ aws-lambda/functions/hsetRedisFinal.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 var mysql = require('promise-mysql');
 var redis = require('redis')
 
-module.exports.hmsetRedis = (event, context, callback) => {
+module.exports.hsetRedisFinal = (event, context, callback) => {
   const client = redis.createClient({ url: 'redis://mywebcahe1.fsgenx.0001.euc1.cache.amazonaws.com:6379' });
   client.on("error", (err) => {
     console.log('error there');
@@ -11,31 +11,20 @@ module.exports.hmsetRedis = (event, context, callback) => {
     callback(err, null);
   });
 
-  const data = JSON.parse(event.body);
-  const arr = [];
-  arr.push(data.hash);
-
-  console.log('hash')
-  console.log(data.hash);
-
-
-  Object.keys(data).forEach(key=>{
-    
-    key !== 'hash' ? (arr.push(key), arr.push(data[key]), console.log(key, data[key])) : null;
-  })
-
-
-  console.log('arr');
-  console.log(arr);
-
-  client.hmset(
-    arr,
+  let user = JSON.parse(event.body);
+  let hash = user.hash;
+  client.hset(
+    hash,
+    `user-${user.id}`,
+    JSON.stringify({
+      id: user.id,
+      name: user.name,
+      age: user.age
+    }),
     function (err, reply) {
       if (err) {
         return res.status(500).json({ message: `There was a problem with saving object to redis`, data: { ...err.errors } });
       }
-      console.log(reply);
-      let reult = {};
       const response = {
         statusCode: 200,
         body: JSON.stringify(reply)
